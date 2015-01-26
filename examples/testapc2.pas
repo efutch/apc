3ce02b1d1e497d40096bac1726c56778ad84157c
Linux Administrator <usuario@linux-vm.(none)>
2015-01-25 20:57:43 -0800

    Eliminar CRLF en archivos de ejemplo

---
+++ examples/testapc2.pas
@@ -0,0 +1,12 @@
+Program prueba(input,output);
+Var
+  a,b,c,e,f,g,x : Integer;
+Var
+  d : Char;
+begin
+   d := #65;
+   if a+b <> c*e then
+     x := a * b + c * f
+   else
+     x := a + -2 * c
+end.
